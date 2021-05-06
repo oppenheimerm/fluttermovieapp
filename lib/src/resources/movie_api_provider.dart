cf49638f54b324bde32885b292cd23061b4a09c7
michieloppenheimer <michieloppenheimer@gmail.com>
2021-05-06 15:45:46 +0100

    addedd details screen

--- lib/src/resources/movie_api_provider.dart
+++ lib/src/resources/movie_api_provider.dart
@@ -1,17 +1,18 @@
 import 'dart:async';
+import 'package:fluttermovieapp/src/models/trailer_model.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../models/item_model.dart';
 
 class MovieApiProvider {
   final _apiKey = '86d908b6a9ec37250f2614e8faf30f67';//'your-api-key';
+  final _baseUrl = 'api.themoviedb.org';
 
   Future<ItemModel> fetchMovieList() async {
-    print("entered");
 
     final response = await http.get(
         Uri.https(
-            'api.themoviedb.org',
+            _baseUrl,
             '/3/movie/popular',
             {'api_key' : {_apiKey}}),
         headers: {
@@ -21,6 +22,25 @@ class MovieApiProvider {
     if (response.statusCode == 200) {
       // If the call to the server was successful, parse the JSON
       return ItemModel.fromJson(json.decode(response.body));
+    } else {
+      // If that call was not successful, throw an error.
+      throw Exception('Failed to load post');
+    }
+  }
+
+  Future<TrailerModel>fetchTrailers(int movieId) async{
+    final response = await http.get(
+        Uri.https(
+            _baseUrl,
+            '/3/movie/$movieId/videos',
+            {'api_key' : {_apiKey}}),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+        });
+    print(response.body.toString());
+    if (response.statusCode == 200) {
+      // If the call to the server was successful, parse the JSON
+      return TrailerModel.fromJson(json.decode(response.body));
     } else {
       // If that call was not successful, throw an error.
       throw Exception('Failed to load post');
--- lib/src/ui/movie_list.dart
+++ lib/src/ui/movie_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fluttermovieapp/src/blocs/movie_detail_bloc_provider.dart';
+import 'package:fluttermovieapp/src/ui/movie_detail.dart';
 import '../models/item_model.dart';
 import '../blocs/movies_bloc.dart';
 
@@ -54,11 +56,36 @@ class _MovieListState extends State<MovieList> {
         gridDelegate:
         new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemBuilder: (BuildContext context, int index) {
-          return Image.network(
-            'https://image.tmdb.org/t/p/w185${snapshot.data
-                .results[index].posterPath}',
-            fit: BoxFit.cover,
+          return GridTile(
+            child: InkResponse(
+              enableFeedback: true,
+              child: Image.network(
+                'https://image.tmdb.org/t/p/w185${snapshot.data
+                    .results[index].posterPath}',
+                fit: BoxFit.cover,
+              ),
+              onTap: () => openDetailPage(snapshot.data, index),
+            ),
           );
         });
   }
+
+  openDetailPage(ItemModel data, int index) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) {
+        return MovieDetailBlocProvider(
+          child: MovieDetail(
+          title: data.results[index].title,
+          posterUrl: data.results[index].backdropPath,
+          description: data.results[index].overview,
+          releaseDate: data.results[index].releaseDate,
+          voteAverage: data.results[index].voteAverage.toString(),
+          movieId: data.results[index].id,
+          ),
+        );
+      }),
+    );
+  }
+
 }
